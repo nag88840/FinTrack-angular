e392630984f4cf8040672d1b96f0bfd86eb3e57f
Nag <Nagarajkrb88840@gmail.com>
2024-11-26 22:42:46 +0530

    commentedd in routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,10 @@ import { ExpenseComponent } from './finance/expense/expense.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
  import { DashboardComponent } from './finance/dashboard/dashboard.component';
-import { AppComponent } from './app.component';
+// import { AppComponent } from './app.component';
 
 const routes: Routes = [
- 
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   {path:"home",component:DashboardComponent},
   {path:"income",component:IncomeComponent},
   {path:"expense",component:ExpenseComponent},
